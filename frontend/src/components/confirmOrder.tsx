a41973074b87a2b3a5669de880d5927fe8311c71
danielolimberio0088 <daniel.olimberio@secret6.com>
2024-05-26 00:11:34 +0800

    add https request for generating invoice

--- frontend/src/components/confirmOrder.tsx
+++ frontend/src/components/confirmOrder.tsx
@@ -3,14 +3,35 @@ import { useParams } from 'react-router-dom';
 import '../App.css'
 
 function ConfirmOrder() {
-    const { paymentId } = useParams();
+    const { orderId } = useParams();
 
-    console.log(paymentId);
+    const generateInvoice = async () => {
+        try {
+          if (orderId == null || orderId == undefined) {
+            throw new Error("Undefined order id");
+          }
+          const response = await fetch(import.meta.env.VITE_API_URL + "order/invoice/" + orderId, {
+            method: "POST",
+            headers: {
+                "Accept": "application/pdf",
+                "Content-Type": "application/pdf",
+            },
+          });
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          await response.json(); 
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }
+
+    console.log(orderId);
     return (
         <div style={{marginTop: 50}}>
             <h2 style={{color : "white"}}>Confirm Order</h2>
             <h4 style={{color : "white"}}>Please print you receipt before closing this page.</h4>
-            <Button>Print your receipt</Button>
+            <Button onClick={generateInvoice}>Print your receipt</Button>
         </div>
         );
     }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -85,7 +85,7 @@ const router = createBrowserRouter([
     loader: fetchUserCartList
   },
   {
-    path: "/confirmOrder/:paymentId",
+    path: "/confirmOrder/:orderId",
     element: <ConfirmOrder/>
   }
 ])
